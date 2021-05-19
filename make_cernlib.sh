22abfc795e4a6ea2c7ce71bc0afbebe73dda314d
cdean-github <cameron.dean@cern.ch>
2021-05-19 00:50:44 -0400

    Removed extra shell commands

--- make_cernlib.sh
+++ make_cernlib.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-mkdir cernlib
+mkdir CERNlib
 cp cernlib-2005-all-new.tgz cernlib/cernlib.2005.corr.tgz
 cp cernlib.2005.corr.2020.12.11.tgz cernlib/cernlib_2005.tgz
 tar -zxvf cernlib.2005.install.2020.12.11.tgz -C cernlib
-cd cernlib/
-#wget https://cernlib.web.cern.ch/download/2005_source/tar/include.tar.gz  
-#wget https://cernlib.web.cern.ch/download/2005_source/tar/gcalor.tar.gz   
-#wget https://cernlib.web.cern.ch/download/2005_source/tar/mathlib32_src.tar.gz
-#wget https://cernlib.web.cern.ch/download/2005_source/tar/minuit32_src.tar.gz  
-#wget https://cernlib.web.cern.ch/download/2005_source/tar/nypatchy_boot.tar.gz 
+cd CERNlib/
 wget https://cernlib.web.cern.ch/download/2005_source/tar/src_Imakefile.tar.gz    
 wget https://cernlib.web.cern.ch/download/2005_source/tar/src_car.tar.gz          
 wget https://cernlib.web.cern.ch/download/2005_source/tar/src_cfortran.tar.gz     
@@ -24,8 +19,5 @@ wget https://cernlib.web.cern.ch/download/2005_source/tar/src_patchy.tar.gz
 wget https://cernlib.web.cern.ch/download/2005_source/tar/src_pawlib.tar.gz       
 wget https://cernlib.web.cern.ch/download/2005_source/tar/src_phtools.tar.gz      
 wget https://cernlib.web.cern.ch/download/2005_source/tar/src_scripts.tar.gz      
-sed -i -e 's/FC=" "/FC=gfortran/g' Install_cernlib
-sed -i -e 's/[ "$GCCVSN" = "3" ]&&FC=g77/#[ "$GCCVSN" = "3" ]&&FC=g77/g' Install_cernlib
-sed -i -e 's/[ "$GCCVSN" = "4" ]&&FC=gfortran/#[ "$GCCVSN" = "4" ]&&FC=gfortran/g' Install_cernlib
 ./Install_cernlib
 source cernlib_env 
